7d1cf485002ed1099cf89f51285d7c52c62f27fd
James Lambert <james.a.lambert@gmail.com>
2022-06-16 23:32:37 +0100

    better serial comms

--- Arduino_FiniteState/Arduino_FiniteState.ino
+++ Arduino_FiniteState/Arduino_FiniteState.ino
@@ -4,77 +4,77 @@ const int digital3 = 50;
 const int digital4 = 51;
 const int digital5 = 52;
 const int digital6 = 53;
-const int digitalOuts[] = {digital1, digital2, digital3, digital4, digital5, digital6};
+const int digital7 = 22;
+const int digital8 = 23;
+const int digital9 = 24;
+const int digital10 = 25;
+
+
+const int digitalOuts[] =   {digital1, digital2, digital3, digital4, digital5,
+                            digital6, digital7, digital8, digital9, digital10, LED_BUILTIN};
 
 #define Start 0
 #define S1 1
 #define S2 2
-#define End 3
 
-char StartSym = '-';
-char EndSym = '^';
+const char StartSym = '-';
+const char EndSym = '^';
 
-int state = End; //start in the end state
+int state = Start; //start in the start state
 int channel = -1;
 int activation = -1;
 
 void setup() {
-    for(int i=0; i<6; i++){
+    for(int i=0; i<11; i++){
         pinMode(digitalOuts[i], OUTPUT);
         digitalWrite(digitalOuts[i], LOW);
     }
-	Serial.begin(250000); // use the same baud-rate as the python side
-	
-// 	flush serial data here?
-//     while (Serial.available()){
-//         Serial.read();
-//     }
-//     Serial.flush();
+    Serial.begin(250000); // use the same baud-rate as the python side
 }
 
 
 void loop(){
-    while (Serial.available()){
-        int newByte = Serial.read();
-        switch (state){
-            case Start:
-//                 Serial.println("S");
-                channel = -1;
-                activation = -1;
+    switch (state){
+        case Start:
+            ledOn(3);
+            channel = -1;
+            activation = -1;
+            if (Serial.available()){
+                int newByte = Serial.read();
                 if ((char) newByte == StartSym || (char) newByte == EndSym){
                     state = Start;
                 } else {
                     channel = (int) newByte;
                     state = S1;
                 }
-                break;
-            case S1:
-//                 Serial.println("A");
+            }
+            break;
+        case S1:
+            ledOn(2);
+            if (Serial.available()){
+                int newByte = Serial.read();
                 if ((char) newByte == StartSym || (char) newByte == EndSym){
                     state = Start;
                 } else {
                     activation = (int) newByte;
                     state = S2;
                 }
-                break;
-            case S2:
-//                 Serial.println("B");
+            }
+            break;
+        case S2:
+            ledOn(1);
+            if (Serial.available()){
+                int newByte = Serial.read();
                 if ((char) newByte == EndSym){
                     setActivation(channel, activation);
                     Serial.print(channel);
                     Serial.println(activation);
-                    state = End;
+                    state = Start;
                 } else {
                     state = Start;
                 }
-                break;
-            case End:
-//                 Serial.println("E");
-                if ((char) newByte == StartSym){
-                    state = Start;
-                }
-                break;
-        }
+            }
+            break;
     }
 }
 
@@ -83,4 +83,11 @@ void setActivation(int channel, int activation){
     
     int a = activation > 0 ? HIGH : LOW;
     digitalWrite(digitalOuts[channel - 1], a);
+}
+
+void ledOn(int j){
+    for(int i=0; i<4; i++){
+        digitalWrite(digitalOuts[6 + i], LOW);
+    }
+    digitalWrite(digitalOuts[6 + j], HIGH);
 }
--- serialWorker.py
+++ serialWorker.py
@@ -1,29 +1,36 @@
 import serial
 import time
 
-MEGA = "/dev/cu.usbmodem141401"
-NANO = "/dev/cu.usbserial-14120"
-modem = MEGA
+MEGA        = {"device" : "/dev/cu.usbmodem141401", "baud" : 250000, "timeout" : 1}
+NANO        = {"device" : "/dev/cu.usbserial-14120", "baud" : 250000, "timeout" : 1}
+TERMINAL    = {"device" : "/dev/ttys005", "baud" : 9600, "timeout" : 1}
 
-baud_rate = 250000
-timeout = 1 #seconds
+modem = MEGA
 
 startSym = "-"
 endSym = "^"
 
 def getSerialConnection():
-    conn = serial.Serial(modem, baud_rate, timeout=timeout)
-    conn.reset_input_buffer()
-    conn.reset_output_buffer()
+    conn = serial.Serial(modem["device"], modem["baud"], timeout=modem["timeout"])
+    time.sleep(3)
+#     conn.write('\r'.encode())
+#     conn.reset_input_buffer()
+#     conn.reset_output_buffer()
+    conn.write(startSym.encode()) #put Arduino into ready state
     return conn
        
 def write(conn, command):
     commandBytes = startSym.encode('ascii') + bytes(command) + endSym.encode('ascii')
     numberOfBytesSent = conn.write(commandBytes)
-#     print(f"serialWorker sent {numberOfBytesSent} bytes: {commandBytes}")
+    print(f"serialWorker sent {numberOfBytesSent} bytes: {commandBytes}")
     
 def read(conn):
-    data = conn.read_until(expected='\n').decode().strip()
+    data = conn.read_until(expected='\r\n')
     if len(data) != 0:
         print(f"serialWorker read {len(data)}: {data}")
-    return data    
+    return data
+    
+def status(conn):
+    status = {"modem" : conn.name, "rate" : conn.baudrate, "isConnected" : conn.is_open,
+            "port": conn.port, "timeout" : conn.timeout}
+    return status
