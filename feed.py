139655988a61b10fec4ea01bfd00adecb28fa3f0
James Lambert <james.a.lambert@gmail.com>
2022-06-27 15:10:17 +0100

    began adding type declarations

--- feed.py
+++ feed.py
@@ -1,3 +1,4 @@
+from typing import Optional, Any
 from enum import Enum
 import sqlite3
 import numpy as np
@@ -5,7 +6,7 @@ import numpy as np
 class Feed():
 # mark _____________________ state _____________________
     @staticmethod
-    def description():
+    def description() -> list[dict[str, Any]]:
         deviceList = Feed.__getDeviceStates()
         for d in deviceList:
             if d['id'] in Feed.__allowedDeviceIDs():
@@ -20,55 +21,66 @@ class Feed():
 
 #mark _____________________ Command Logic _____________________
     @staticmethod
-    def commandsFrom(commandData):
+    def commandsFrom(commandData: str) -> Optional[list[tuple[int,int]]]:
         """
         returned values will be sent to the serial device
         """
         try:
-            intent = Feed.__intentFromString(commandData)
+            intent: Optional[str] = Feed.__intentFromString(commandData)
         except:
             return None
 
         if intent == "toggle":
+            commands: Optional[list[tuple[int,int]]]
             commands = Feed.__getToggleCommandsFor(commandData)
-            if commands != None:
+            if not commands is None:
                 Feed.__recordActivation(commands)
                 return commands
             else:
                 return None
         elif intent == "updateChannel":
-            command = Feed.__getChannelUpdateCommandFor(commandData)
-            Feed.__recordChannel(*command)
+            deviceID: int
+            newChannel: int
+            deviceID, newChannel = Feed.__getChannelUpdateCommandFor(commandData)
+            Feed.__recordChannel(deviceID, newChannel)
             return None
         else:
             return None
             
         
     @staticmethod
-    def commandsFromSerial(data):
+    def commandsFromSerial(data: str):
 #     todo: this will receive the repeated commands from the arduino,
 #     turn the data into commands, and then commit those commands to the database.
         if len(data) > 0:
             print(f"Feed got serial data: {data}")
+        
 
     @staticmethod
-    def __getToggleCommandsFor(commandData):
-        deviceID = commandData.split(",")[1]
+    def __getToggleCommandsFor(commandData: str) -> Optional[list[tuple[int,int]]]:
+        deviceID : int
+        activation: int
+        devicesToToggle: list[int]
+        descendants: list[int]
+        channels: set[int]
+        commands: list[tuple[int,int]]
         
-        if not int(deviceID) in Feed.__allowedDeviceIDs():
+        deviceID = int(commandData.split(",")[1])
+        devicesToToggle = [deviceID]
+        
+        if not deviceID in Feed.__allowedDeviceIDs():
             return None
             
         conn = Feed.__getDBConnection()
-        currentActivation = Feed.__activationFor(deviceID, conn)
+        currentActivation: int = Feed.__activationFor(deviceID, conn)
         
-        devicesToToggle = [deviceID]
         if currentActivation == 0:
+#           switch on parents
             activation = 1
-#             switch on parents
             devicesToToggle += Feed.__getAncestorDeviceIDs(deviceID, conn)
         else:
+#           switch off children too
             activation = 0
-#             switch off children too
             descendants = Feed.__getDescendantDeviceIDs(deviceID, conn)
             devicesToToggle += descendants
         
@@ -80,15 +92,18 @@ class Feed():
 
  
     @staticmethod
-    def __getChannelUpdateCommandFor(commandData):
-        deviceID, newChannel = commandData.split(",")[1:3]
+    def __getChannelUpdateCommandFor(commandData: str) -> tuple[int, int]:
+        print(f"updating: {commandData}")
+        deviceAndChannel = commandData.split(",")
+        deviceID: int = int(deviceAndChannel[1])
+        newChannel: int = int(deviceAndChannel[2])
         return deviceID, newChannel
 
     @staticmethod
-    def __intentFromString(data):
+    def __intentFromString(data: str) -> Optional[str]:
         try:
-            data = data.split(",")
-            return data[0]
+            words: list[str] = data.split(",")
+            return words[0]
         except:
             return None
 
@@ -96,7 +111,7 @@ class Feed():
 
 #MARK  _____________________ device dependency management _____________________
     @staticmethod
-    def __allowedDeviceIDs():
+    def __allowedDeviceIDs() -> set[int]:
         """
         returns the children of currently active devices
         """
@@ -105,31 +120,30 @@ class Feed():
 where devices.channel = outputs.channel and outputs.activation = 1"""
         cur = conn.cursor()
         activatedDeviceIDsRows = cur.execute(activatedDevicesCommand).fetchall()
-        activatedDeviceIDs = [row['id'] for row in activatedDeviceIDsRows]
+        activatedDeviceIDs: list[int] = [row['id'] for row in activatedDeviceIDsRows]
         
         cur = conn.cursor()
         graphRows = cur.execute("select * from graph;").fetchall()
         graphRows = [row for row in graphRows if row['parentID'] in activatedDeviceIDs]
         #         will always include the header parentID as the first result, ignore it.
-        enabledDeviceIDs = [int(k) for row in graphRows for k in row.keys() if row[k] == 1 and k != 'parentID']
+        enabledDeviceIDs: list[int] = [int(k) for row in graphRows for k in row.keys() if row[k] == 1 and k != 'parentID']
         
         cur = conn.cursor()
-        initialDeviceID = cur.execute("select id from devices where title = 'main power'").fetchone()
+        initialDeviceID: list[int] = cur.execute("select id from devices where title = 'main power'").fetchone()
         enabledDeviceIDs += initialDeviceID
         
         conn.close()
-        return enabledDeviceIDs
+        return set(enabledDeviceIDs)
 
 
     @staticmethod
-    def __getAncestorDeviceIDs(deviceID, conn):
+    def __getAncestorDeviceIDs(deviceID: int, conn) -> list[int]:
         """
         returns the parents of a given device
         """
-        deviceID = int(deviceID)
         cur = conn.cursor()
         rows = cur.execute("select * from graph").fetchall()
-        deviceIDs = [row['parentID'] for row in rows]
+        deviceIDs: list[int] = [row['parentID'] for row in rows]
 
         # row vector
         deviceIDVec = np.array([1 if e == deviceID else 0 for e in deviceIDs])
@@ -145,14 +159,13 @@ where devices.channel = outputs.channel and outputs.activation = 1"""
 
 
     @staticmethod
-    def __getDescendantDeviceIDs(deviceID, conn):
+    def __getDescendantDeviceIDs(deviceID: int, conn) -> list[int]:
         """
         returns the children of a given device
         """
-        deviceID = int(deviceID)
         cur = conn.cursor()
         rows = cur.execute("select * from graph").fetchall()
-        deviceIDs = [row['parentID'] for row in rows]
+        deviceIDs: list[int] = [row['parentID'] for row in rows]
     
         # row vector
         deviceIDVec = np.array([1 if e == deviceID else 0 for e in deviceIDs])
@@ -163,9 +176,8 @@ where devices.channel = outputs.channel and outputs.activation = 1"""
             descendants |= descendants @ graphVec
         
         descendants = [1 if e > 0 else 0 for e in descendants]
-        descentantIDs = np.multiply(deviceIDs, descendants)
-        descentantIDs = descentantIDs[descentantIDs > 0].tolist()
-        return descentantIDs
+        descendantIDs = np.multiply(deviceIDs, descendants)
+        return descendantIDs[descendantIDs > 0].tolist()
 
 
 
@@ -173,21 +185,21 @@ where devices.channel = outputs.channel and outputs.activation = 1"""
 #MARK _____________________ Database actions _____________________
 # TODO: adding a new device also needs to add it to the graph
     @staticmethod
-    def recordNewDevice(title):
+    def recordNewDevice(title: str):
         conn = Feed.__getDBConnection()
         Feed.__addDevice(title, conn)
         conn.commit()
         conn.close()
         
     @staticmethod
-    def removeDevice(deviceID):
+    def removeDevice(deviceID: int):
         conn = Feed.__getDBConnection()
         Feed.__deleteDevice(deviceID, conn)
         conn.commit()
         conn.close()
         
     @staticmethod
-    def __recordActivation(commands):
+    def __recordActivation(commands: list[tuple[int, int]]):
         conn = Feed.__getDBConnection()
 
         for channel, activation in commands:
@@ -197,14 +209,14 @@ where devices.channel = outputs.channel and outputs.activation = 1"""
         conn.close()
 
     @staticmethod
-    def __recordChannel(deviceID, newChannel):
+    def __recordChannel(deviceID: int, newChannel: int):
         conn = Feed.__getDBConnection()
         Feed.__setChannel(deviceID, newChannel, conn)
         conn.commit()
         conn.close()
 
     @staticmethod
-    def __getDeviceStates():
+    def __getDeviceStates() -> list[dict[str, Any]]:
         """
         returns all device activations
         """
@@ -216,7 +228,6 @@ where devices.channel = outputs.channel and outputs.activation = 1"""
         return deviceStates
         
 
-    
 #MARK _____________________ db access helpers_____________________
 
     @staticmethod
@@ -226,42 +237,42 @@ where devices.channel = outputs.channel and outputs.activation = 1"""
         return conn
 
     @staticmethod
-    def __activationFor(deviceID, conn):
+    def __activationFor(deviceID: int, conn) -> int:
         command = """
 select activation
 from devices, outputs
 where devices.channel = outputs.channel
 and devices.id = ?;
 """
-        activations = conn.execute(command, deviceID).fetchone()
+        activations = conn.execute(command, str(deviceID)).fetchone()
         print(f"db got activation: {activations['activation']}")
         return activations['activation']
 
     @staticmethod
-    def __setActivation(activation, channel, conn):
+    def __setActivation(activation: int, channel: int, conn):
         command = "update outputs set activation = ? where channel = ?;"
         conn.execute(command, [str(activation), str(channel)])
         print(f"updating database: {activation} for channel: {channel}")
         
     @staticmethod
-    def __getChannelFor(deviceID, conn):
+    def __getChannelFor(deviceID: int, conn) -> int:
         command = "select channel from devices where id = ?"
-        row = conn.execute(command, (deviceID,)).fetchone()
+        row = conn.execute(command, (str(deviceID),)).fetchone()
         return row['channel']
         
     @staticmethod
-    def __setChannel(deviceID, newChannel, conn):
-        conn.execute("UPDATE devices set channel = ? WHERE id is ?", (newChannel, deviceID))
+    def __setChannel(deviceID: int, newChannel: int, conn):
+        conn.execute("UPDATE devices set channel = ? WHERE id is ?", (str(newChannel), str(deviceID)))
         print(f"updated db, set device {deviceID} to channel: {newChannel}")
         
     @staticmethod
-    def __addDevice(title, conn):
+    def __addDevice(title: str, conn):
         conn.execute("INSERT INTO devices (title) VALUES (?)", (title,))
         print(f"updating database: added {title}")
         
     @staticmethod
-    def __deleteDevice(deviceID, conn):
-        conn.execute("DELETE FROM devices WHERE id = ?", (deviceID,))
+    def __deleteDevice(deviceID: int, conn):
+        conn.execute("DELETE FROM devices WHERE id = ?", (str(deviceID),))
         print(f"deleted device {deviceID}")
         
     
